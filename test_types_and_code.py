fa276debd912007f67e629a2753df2befa120396
Pierre Glaser <pierreglaser@msn.com>
2018-12-06 11:01:05 +0100

    ENH more thorough tests

--- test_types_and_code.py
+++ test_types_and_code.py
@@ -1,10 +1,19 @@
-#!/home/pierreglaser/dev_python/bin/python3.8
+#!/home/pierreglaser/c_pickle_python/bin/python3.8
 # -*- coding: utf-8 -*-
 import pickle
+import pickletools
 import types
 
 def f():
     pass
+
+
+def g():
+    a = 1
+
+    def h():
+        return a
+    return h
 
 
 print(pickle.dumps(f.__code__))
@@ -12,3 +21,12 @@ print(pickle.loads(pickle.dumps(f.__code__)))
 
 print(pickle.dumps(types.FunctionType))
 print(pickle.loads(pickle.dumps(types.FunctionType)))
+
+closure_func = g()
+
+print(g.__code__.co_cellvars)
+print(g.__code__.co_freevars)
+# print(pickletools.dis(pickle.dumps(g.__code__)))
+u = pickle.loads(pickle.dumps(g.__code__))
+print(u.co_cellvars)
+print(u.co_freevars)
